8a6c29b90549c2bd4aca2fb974ef05f272d54318
Krish Mittal <krishmittal212005@gmail.com>
2024-05-23 17:06:29 +0530

    All Files Needed

---
+++ InventoryManagement/Models/IMProduct.swift
@@ -0,0 +1,8 @@
+//
+//  IMProduct.swift
+//  InventoryManagement
+//
+//  Created by Krish Mittal on 23/05/24.
+//
+
+import Foundation
---
+++ InventoryManagement/Utilities/Components/IMProductCardView.swift
@@ -0,0 +1,18 @@
+//
+//  IMProductCardView.swift
+//  InventoryManagement
+//
+//  Created by Krish Mittal on 23/05/24.
+//
+
+import SwiftUI
+
+struct IMProductCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    IMProductCardView()
+}
