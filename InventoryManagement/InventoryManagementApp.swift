a81397c768676d11dd67b24a70609977e0d8dfbf
Krish Mittal <krishmittal212005@gmail.com>
2024-05-23 16:25:06 +0530

    Initial Commit

---
+++ InventoryManagement/InventoryManagementApp.swift
@@ -0,0 +1,17 @@
+//
+//  InventoryManagementApp.swift
+//  InventoryManagement
+//
+//  Created by Krish Mittal on 23/05/24.
+//
+
+import SwiftUI
+
+@main
+struct InventoryManagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
